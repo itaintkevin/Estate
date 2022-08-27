d08bec345e9e0a9e5bbb7810a97c1fcbcf82acbe
itaintkevin <kpjtofficial@gmail.com>
2022-08-27 13:00:42 +0530

    New Fixes

--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -1,6 +1,6 @@
 const Marketplace = () => {
     return(
-        <div className="flex flex-col items-center justify-center h-screen bg-cover bg-[url('/home/kev/Documents/Coding/Live-Projects/tedx-website/src/background.svg')]">
+        <div className="flex flex-col items-center justify-center h-screen bg-cover bg-[url('components/background.svg')]">
             {/* <div className="text-3xl text-white font-semibold w-full px-64 my-8">
               
             </div> */}
